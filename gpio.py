fd682067f8d06ee3174884ab7894082abb790e81
Coulibaly Zie Mamadou <17972148+zie225@users.noreply.github.com>
2021-10-27 04:59:18 +0200

    Add files via upload

---
+++ gpio.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 27 04:53:41 2021
+
+@author: Mc Zie
+"""
+# Start by importing the libraries we want to use
+
+import RPi.GPIO as GPIO # This is the GPIO library we need to use the GPIO pins on the Raspberry Pi
+import smtplib # This is the SMTP library we need to send the email notification
+import time # This is the time library, we need this so we can use the sleep function
+
+# Set our GPIO numbering to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Define the GPIO pin that we have our digital output from our sensor connected to
+channel = 17
+# Set the GPIO pin to an input
+GPIO.setup(channel, GPIO.IN)
+
+# This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+
+
+# This is an infinte loop to keep our script running
+while True:
+	# This line simply tells our script to wait 0.1 of a second, this is so the script doesnt hog all of the CPU
+	time.sleep(0.1)
