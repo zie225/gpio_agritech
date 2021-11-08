101d7a7ca1e4ece8fc12dadb14b2372f07216a8e
Coulibaly Zie Mamadou <17972148+zie225@users.noreply.github.com>
2021-11-08 02:26:57 +0100

    Add files via upload

---
+++ Board_reader_code_Prediction.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  3 14:00:34 2021
+
+@author: Mc Zie
+"""
+
+import time
+import board
+import busio
+i2c = busio.I2C(board.SCL, board.SDA)
+
+import adafruit_ads1x15.ads1015 as ADS
+#import adafruit_ads1x15.ads1115 as ADS
+
+from adafruit_ads1x15.analog_in import AnalogIn
+
+
+slope = 2.48; #slope from linear fit
+intercept = -0.72 # intercept from linear fit
+
+ads = ADS.ADS1015(i2c)
+
+chan = AnalogIn(ads, ADS.P0)
+#voltage=chan.voltage
+#vol_water_cont = ((1.0/chan.voltage)*slope)+intercept #calc of theta_v (vol. water content)
+
+while True: 
+    try: 
+        print( 'voltage:')
+        print(chan.voltage)
+        
+        vol_water_cont = ((1.0/chan.voltage)*slope)+intercept #calc of theta_v (vol. water content)
+
+        print(" V, Theta_v: ",'cm^3/cm^3')
+        print(vol_water_cont)
+        
+        time.sleep(.5) 
+ 
+    except KeyboardInterrupt: 
+        break 
+    except IOError: 
+        print ("Error") 
